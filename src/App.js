f7beeed98486080aa554b976ce150e8f08b7fb2c
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-12 18:54:30 +0200

    feat: Add Loader and Error component to Main

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
 
 const initialState = {
   questions: [],
@@ -27,7 +29,7 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -40,8 +42,8 @@ export default function App() {
       <Header />
 
       <Main>
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
       </Main>
     </div>
   );
