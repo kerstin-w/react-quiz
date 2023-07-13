3d833744f4a5d131f331379c3d43a8fb4372b9f6
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-13 15:27:40 +0200

    docs: Create comment for server.js

--- functions/server.js
+++ functions/server.js
@@ -1,3 +1,11 @@
+/**
+ * The above function exports an asynchronous handler that returns a JSON response containing a list of
+ * questions.
+ * @returns The handler function is returning an object with two properties: "statusCode" and "body".
+ * The "statusCode" property is set to 200, indicating a successful response, and the "body" property
+ * is set to the JSON string representation of the "questions" array from the "./data/questions.json"
+ * file.
+ */
 export default async function handler() {
   const questions = require("./data/questions.json");
   return {
