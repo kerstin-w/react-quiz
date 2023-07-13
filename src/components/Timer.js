4541de50edaa0ead0c57b488c99e5b02c3cf5d5a
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-13 15:29:00 +0200

    docs: Create comment for Timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,8 +1,17 @@
 import { useEffect } from "react";
 
+/**
+ * The Timer function is a React component that displays a countdown timer and updates the timer every
+ * second.
+ * @returns The Timer component is returning a div element with the class name "timer". Inside the div,
+ * it displays the minutes and seconds remaining in the format "mm:ss". If the minutes or seconds are
+ * less than 10, a leading "0" is added to the display.
+ */
 function Timer({ dispatch, secondsRemaining }) {
   const mins = Math.floor(secondsRemaining / 60);
   const secs = secondsRemaining % 60;
+
+  /* The `useEffect` hook is used to start a timer that updates the countdown every second. */
   useEffect(
     function () {
       const id = setInterval(function () {
@@ -12,6 +21,7 @@ function Timer({ dispatch, secondsRemaining }) {
     },
     [dispatch]
   );
+
   return (
     <div className="timer">
       {mins < 10 && "0"}
