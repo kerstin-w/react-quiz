2295d9492388c9e6dfbc526ed0f8a7d753d63f98
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-13 13:55:09 +0200

    docs: Add comment to Question component

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,9 @@
 import Options from "./Options";
 
+/**
+ * The Question component renders a question and its options, and allows the user to select an answer.
+ * @returns The Question component is being returned.
+ */
 function Question({ question, dispatch, answer }) {
   return (
     <div>
