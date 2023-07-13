adeaa62579a32f09061585d76d39b135ae10dd41
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-13 13:57:23 +0200

    docs: Add comment to Options component

--- src/components/Options.js
+++ src/components/Options.js
@@ -1,3 +1,8 @@
+/**
+ * The Options function renders a set of buttons representing multiple choice options for a question,
+ * and handles user interaction and state updates.
+ * @returns The Options component is being returned.
+ */
 function Options({ question, dispatch, answer }) {
   const hasAnswered = answer !== null;
   return (
