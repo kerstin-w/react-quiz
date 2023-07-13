f4c984e3b4f6c6aff9c0a45708cad8b4921d5018
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-13 16:42:28 +0200

    fix: Change path for questions.json

--- netlify/functions/server.js
+++ netlify/functions/server.js
@@ -7,7 +7,7 @@
  * file.
  */
 exports.handler = async (event, context) => {
-  const questions = require("./data/questions.json");
+  const questions = require("../data/questions.json");
   return {
     statusCode: 200,
     body: JSON.stringify(questions),
