8f656e4e96d54ed50186a9ca2fc35291a320dc44
Kerstin <93425993+kerstin-w@users.noreply.github.com>
2023-07-13 13:54:14 +0200

    docs: Add comment to NextButton component

--- src/components/NextButton.js
+++ src/components/NextButton.js
@@ -1,3 +1,8 @@
+/**
+ * The NextButton component renders a button that triggers a dispatch action when clicked, but only if
+ * the answer is not null.
+ * @returns The NextButton component is being returned.
+ */
 function NextButton({ dispatch, answer }) {
   if (answer === null) return null;
   return (
